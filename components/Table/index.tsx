5e8e8576e016a896f388838ca3e619c60279186d
Nicholas Wagner <nclwgn@gmail.com>
2022-09-14 18:13:18 -0300

    Create Table component

---
+++ components/Table/index.tsx
@@ -0,0 +1,76 @@
+import { HTMLProps } from "react";
+
+interface TableProps extends Omit<HTMLProps<HTMLDivElement>, 'className'> {}
+
+function Table({
+  children,
+  ...rest
+}: TableProps) {
+  return (
+    <div
+      className='table border-2 border-collapse border-spacing-0 border-white/10 w-full bg-black/5 text-sm text-left'
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+interface TableHeadProps extends Omit<HTMLProps<HTMLDivElement>, 'className'> {}
+
+function TableHead({
+  children,
+  ...rest
+}: TableHeadProps) {
+  return (
+    <div
+      className='table-header-group border-b-2 border-white/10 bg-black/10 uppercase'
+      {...rest}
+    >
+      <TableRow>
+        {children}
+      </TableRow>
+    </div>
+  );
+}
+
+interface TableRowProps extends Omit<HTMLProps<HTMLDivElement>, 'className'> {}
+
+function TableRow({
+  children,
+  ...rest
+}: TableRowProps) {
+  return (
+    <div
+      className='table-row border-b-2 border-white/5'
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+interface TableCellProps extends Omit<HTMLProps<HTMLDivElement>, 'className'> {
+  text?: 'left' | 'center' | 'right'
+}
+
+function TableCell({
+  children,
+  text = 'left',
+  ...rest
+}: TableCellProps) {
+  return (
+    <div
+      className={`table-cell py-2 px-10 text-${text}`}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+Table.Head = TableHead;
+Table.Row = TableRow;
+Table.Cell = TableCell;
+
+export { Table };
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,3 +1,5 @@
+import { Table } from "../components/Table";
+
 export default function Products() {
   return (
     <div className='container mx-auto'>
@@ -6,45 +8,41 @@ export default function Products() {
         <button className='success'>Adicionar</button>
       </div>
 
-      <div className='rounded-lg'>
-        <table className='table-auto border-2 border-collapse border-spacing-0 border-white/10 w-full bg-black/5 text-sm text-left my-5'>
-          <thead className='border-b-2 border-white/10 bg-black/10 uppercase'>
-            <th className='py-2 px-10'>Produto</th>
-            <th className='py-2 px-10'>Nº rastreadores</th>
-            <th className='py-2 px-10'></th>
-          </thead>
-          <tbody>
-            <tr className='border-b-2 border-white/5'>
-              <td className='py-2 px-10'>Headset Corsair Void Elite Gaming Wireless</td>
-              <td className='py-2 px-10'>2</td>
-              <td className='py-2 px-10 text-right'>
-                <button className='danger sm'>X</button>
-              </td>
-            </tr>
-            <tr className='border-b-2 border-white/5'>
-              <td className='py-2 px-10'>Headset Corsair Void Elite Gaming Wireless</td>
-              <td className='py-2 px-10'>2</td>
-              <td className='py-2 px-10 text-right'>
-                <button className='danger sm'>X</button>
-              </td>
-            </tr>
-            <tr className='border-b-2 border-white/5'>
-              <td className='py-2 px-10'>Headset Corsair Void Elite Gaming Wireless</td>
-              <td className='py-2 px-10'>2</td>
-              <td className='py-2 px-10 text-right'>
-                <button className='danger sm'>X</button>
-              </td>
-            </tr>
-            <tr className='border-b-2 border-white/5'>
-              <td className='py-2 px-10'>Headset Corsair Void Elite Gaming Wireless</td>
-              <td className='py-2 px-10'>2</td>
-              <td className='py-2 px-10 text-right'>
-                <button className='danger sm'>X</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <Table>
+        <Table.Head>
+          <Table.Cell>Produto</Table.Cell>
+          <Table.Cell>Nº rastreadores</Table.Cell>
+          <Table.Cell />
+        </Table.Head>
+        <Table.Row>
+          <Table.Cell>Headset Corsair Void Elite Gaming Wireless</Table.Cell>
+          <Table.Cell>2</Table.Cell>
+          <Table.Cell text='right'>
+            <button className='danger sm'>X</button>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Headset Corsair Void Elite Gaming Wireless</Table.Cell>
+          <Table.Cell>2</Table.Cell>
+          <Table.Cell text='right'>
+            <button className='danger sm'>X</button>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Headset Corsair Void Elite Gaming Wireless</Table.Cell>
+          <Table.Cell>2</Table.Cell>
+          <Table.Cell text='right'>
+            <button className='danger sm'>X</button>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Headset Corsair Void Elite Gaming Wireless</Table.Cell>
+          <Table.Cell>2</Table.Cell>
+          <Table.Cell text='right'>
+            <button className='danger sm'>X</button>
+          </Table.Cell>
+        </Table.Row>
+      </Table>
 
       <div className='flex justify-end'>
         <button className='success'>Adicionar</button>
